aa85f6e0254b1592943f455ede5b08e7367fbe34
James <jgrove792@gmail.com>
2021-01-09 14:45:11 +0000

    Exercise Completed

--- Exercises/Computing_Loan_Payments.py
+++ Exercises/Computing_Loan_Payments.py
@@ -1,9 +1,32 @@
-def month_pay(RATE, YEARS, AMOUNT):
+def month_pay(rate, years, amount):
 
-    A = AMOUNT * RATE
-    B = 1 - (1/(1+(RATE/12) * (YEARS * 12)))
+    a = amount * rate
+    b = 1 - (1 / ((1 + rate) ** (years * 12)))
 
-    return A / B
+    return print("£ {:.2f}".format(a // b))
+
 
 if __name__ == '__main__':
-    input()
+
+    while True:
+        try:
+            RATE = float(input('Please enter the monthly interest rate as a decimal... '))
+            break
+        except ValueError:
+            print('Incorrect, try entering a whole value such as 0.10.')
+
+    while True:
+        try:
+            YEARS = int(input('Please enter the number of years as a whole integer... '))
+            break
+        except ValueError:
+            print('Incorrect, try entering a whole value such as 2.')
+
+    while True:
+        try:
+            AMOUNT = float(input('Please enter the amount... '))
+            break
+        except ValueError:
+            print('Incorrect, try entering a numerical value such as 10000.')
+
+    month_pay(RATE, YEARS, AMOUNT)
